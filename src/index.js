b070675d82bb0a1bd4f62502698b8bfed4070192
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-08-22 10:18:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.querySelector('#create-task-form');
+  const input = document.querySelector('#new-task-description');
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const li = document.createElement('li');
+  const tasks = document.querySelector('#tasks');
+  const btn = document.createElement('button');
+  btn.addEventListener('click', (e)=> {
+    e.target.parentNode.remove();
+  })
+  btn.textContent = 'x';
+  li.textContent = `${input.value} `;
+  li.appendChild(btn);
+  tasks.appendChild(li)
+  e.target.input.value
+   }   
+)})
+
+
+
